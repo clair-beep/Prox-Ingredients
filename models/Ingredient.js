c1cd4c337044f08830092c3c111d1def95baacee
the-armistead <armistead2000@gmail.com>
2023-02-01 15:46:34 -0500

    added the posibility to enter the ingredients in 2 languages,implemented this through the ingredients model

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -1,20 +1,27 @@
 const mongoose = require('mongoose');
 
 const IngredientSchema = new mongoose.Schema({
-	title: {
+	englishTitle: {
 		type: String,
 		trim: true,
 		required: [true, 'Please add a ingredient title']
+	},
+
+	spanishTitle: {
+		type: String,
+		trim: true
 	},
 
 	description: {
 		type: String,
 		required: [true, 'Please add a description']
 	},
+
 	references: {
 		type: String,
 		required: [true, 'Please add the references used']
 	},
+
 	product: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Product'

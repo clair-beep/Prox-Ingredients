e91339d73c2ed05b63c86e30c42c696bd30d212c
the-armistead <armistead2000@gmail.com>
2023-02-05 10:23:01 -0500

    assigned id to json data

--- seeder.js
+++ seeder.js
@@ -2,46 +2,54 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const colors = require('colors');
 const dotenv = require('dotenv');
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 //Load models
-const Product = require('./models/Product')
+const Product = require('./models/Product');
+const Ingredient = require('./models/Ingredient');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI);
 
 //Read JSON files
-const products = JSON.parse(fs.readFileSync(`${__dirname}/_data-samples/products.json`, 'utf-8'));
+const products = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data-samples/products.json`, 'utf-8')
+);
+
+const ingredients = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data-samples/ingredients.json`, 'utf-8')
+);
 
 //Import into DB
 const importData = async () => {
-    try {
-        await Product.create(products);
+	try {
+		await Product.create(products);
+		await Ingredient.create(ingredients);
 
-        console.log('Data Imported...'.green.inverse);
-        process.exit();
-    } catch (err) {
-        console.log(err);
-    }
-}
+		console.log('Data Imported...'.green.inverse);
+		process.exit();
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 //Delete data
 const deleteData = async () => {
-    try {
-        await Product.deleteMany();
-
-        console.log('Data Destroyed...'.red.inverse);
-        process.exit();
-    } catch (err) {
-        console.log(err);
-    }
-}
+	try {
+		await Product.deleteMany();
+		await Ingredient.deleteMany();
+		console.log('Data Destroyed...'.red.inverse);
+		process.exit();
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 if (process.argv[2] === '-i') {
-    importData();
+	importData();
 } else if (process.argv[2] === '-d') {
-    deleteData();
+	deleteData();
 }

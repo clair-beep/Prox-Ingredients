69dbb3e3eaa82e4a3cffd8a10ebd7cd6a8621abc
the-armistead <armistead2000@gmail.com>
2023-02-07 15:59:27 -0500

    last stage of product json data

--- seeder.js
+++ seeder.js
@@ -16,40 +16,42 @@ mongoose.connect(process.env.MONGO_URI);
 
 //Read JSON files
 const products = JSON.parse(
-	fs.readFileSync(`${__dirname}/_data-samples/products.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/_data-samples/products.json`, 'utf-8')
 );
 
 const ingredients = JSON.parse(
-	fs.readFileSync(`${__dirname}/_data-samples/ingredients.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/_data-samples/ingredients.json`, 'utf-8')
 );
 
 //Import into DB
 const importData = async () => {
-	try {
-		await Product.create(products);
-		await Ingredient.create(ingredients);
+  try {
+    await Product.create(products);
+    await Ingredient.create(ingredients);
 
-		console.log('Data Imported...'.green.inverse);
-		process.exit();
-	} catch (err) {
-		console.log(err);
-	}
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 //Delete data
 const deleteData = async () => {
-	try {
-		await Product.deleteMany();
-		await Ingredient.deleteMany();
-		console.log('Data Destroyed...'.red.inverse);
-		process.exit();
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    await Product.deleteMany();
+    await Ingredient.deleteMany();
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 if (process.argv[2] === '-i') {
-	importData();
+  importData();
 } else if (process.argv[2] === '-d') {
-	deleteData();
+  deleteData();
 }
+
+const num = 123123;

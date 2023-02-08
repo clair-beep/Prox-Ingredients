fcbe4e49eec07a6b49acbf2e6084df4eb67578db
the-armistead <armistead2000@gmail.com>
2023-02-08 14:08:09 -0500

    this commit is before troubleshooting json data file

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -1,31 +1,35 @@
 const mongoose = require('mongoose');
 
 const IngredientSchema = new mongoose.Schema({
-	englishTitle: {
-		type: String,
-		trim: true,
-		required: [true, 'Please add a ingredient title']
-	},
+  englishTitle: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a ingredient title'],
+    match: [
+      /^[a-zA-Z0-9 áéíóúüñ]+$/,
+      'Name can only contain alphanumeric characters and spaces'
+    ]
+  },
 
-	spanishTitle: {
-		type: String,
-		trim: true
-	},
+  spanishTitle: {
+    type: String,
+    trim: true
+  },
 
-	description: {
-		type: String,
-		required: [true, 'Please add a description']
-	},
+  description: {
+    type: String,
+    required: [true, 'Please add a description']
+  },
 
-	references: {
-		type: String,
-		required: [true, 'Please add the references used']
-	},
+  references: {
+    type: String,
+    required: [true, 'Please add the references used']
+  },
 
-	product: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Product'
-	}
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product'
+  }
 });
 
 module.exports = mongoose.model('ingredient', IngredientSchema);

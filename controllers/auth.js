d5fa916ad847e840a2d15218c6015b8b5eb1c557
unknown <armistead2000@gmail.com>
2023-03-17 14:52:11 +0100

    encryption implemented for the password of users during the registration process /bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,5 +8,15 @@ const User = require('../models/User');
 // @access Public
 
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
   res.status(200).json({ success: true });
 });

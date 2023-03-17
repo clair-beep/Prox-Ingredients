b55b96a8adf1909c10894ad237c71dd494018322
unknown <armistead2000@gmail.com>
2023-03-17 21:44:22 +0100

    created log in controller so users can log in with email/ password

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 
 // add a header with:
 // @description Register User
-// @route GET /api/auth/register
+// @route POST /api/v1/auth/register
 // @access Public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -17,6 +17,39 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role
   });
+
+  //Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+// add a header with:
+// @description Login User
+// @route POST /api/v1/auth/login
+// @access Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validates email &password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  //Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  ///Check if password amtches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
 
   //Create token
   const token = user.getSignedJwtToken();

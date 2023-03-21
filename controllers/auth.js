4bcd3bf1c9755f525154f23a48eefca57e45e258
unknown <armistead2000@gmail.com>
2023-03-21 17:35:24 +0100

    added sendEmail to utils to be able to send emails whenever a user forgot their password and pa ir it with mailtrap to test the recipien of email in testing mode

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 // add a header with:
@@ -51,19 +52,6 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
-// add a header with:
-// @description Get current logged in user
-// @route GET /api/v1/auth/me
-// @access Private
-//
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
-
 // @desc      Forgot password
 // @route     GET /api/v1/auth/forgotpassword
 // @access    Public
@@ -80,10 +68,29 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
+  //Create reset url
+  const resetUrl = `${req.protocol}//${req.get(
+    'host',
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    });
+    res.status(200).json({ success: true, data: 'Email sent' });
+  } catch (err) {
+    console.log(err);
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
 });
 
 //Get token from model, create cookie and send response
@@ -100,9 +107,17 @@ const sendTokenResponse = (user, statusCode, res) => {
   if (process.env.Node_ENV === 'production') {
     options.secure = true;
   }
-
-  res.status(statusCode).cookie('token', token, options).json({
-    success: true,
-    token,
-  });
 };
+
+// add a header with:
+// @description Get current logged in user
+// @route GET /api/v1/auth/me
+// @access Private
+//
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});

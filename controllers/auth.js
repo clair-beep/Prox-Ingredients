72a7233ae4221682b0502e6c097eaeb6af8e0367
unknown <armistead2000@gmail.com>
2023-03-22 16:54:31 +0100

    added the /update password route Update logged in user Password, send in the body currentPassword and newPassword, additionally added /update details route that allow users to update details related to their user data

--- controllers/auth.js
+++ controllers/auth.js
@@ -55,7 +55,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 
 // add a header with:
 // @description Get current logged in user
-// @route GET /api/v1/auth/me
+// @route POST /api/v1/auth/me
 // @access Private
 //
 exports.getMe = asyncHandler(async (req, res, next) => {
@@ -66,8 +66,48 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc      UPDATE user details
+// @route     PUT /api/v1/auth/updatedetails
+// @access    Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  // user is already available in req due to the protect middleware
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc      Update password
+// @route     PUT /api/v1/auth/updatepassword
+// @access    Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  // user is already available in req due to the protect middleware
+  //const user = req.user;
+  const user = await User.findById(req.user.id).select('+password');
+
+  //Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 // @desc      Forgot password
-// @route     GET /api/v1/auth/forgotpassword
+// @route     POST /api/v1/auth/forgotpassword
 // @access    Public
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
   // user is already available in req due to the protect middleware
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -16,6 +18,8 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
